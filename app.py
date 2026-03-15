26ebc51bc7348d2fa43ae56f0d9fca7ee76bd57b
Solo <solo@example.com>
2026-03-15 17:01:18 +0530

    Bypass YouTube bot detection with mimic headers

--- app.py
+++ app.py
@@ -38,6 +38,12 @@ def get_info():
         'youtube_include_hls_manifest': True,
         'nocheckcertificate': True,
         'no_playlist': True,
+        'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+        'http_headers': {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Sec-Fetch-Mode': 'navigate',
+        },
     }
     
     try:
